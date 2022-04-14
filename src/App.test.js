6d01e789861c52ea98ad041851556210b3367e9f
P S <ps0hakai.tenshi@gmail.com>
2022-04-14 16:59:55 +0200

    [#6] Added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import {  render, screen } from "./utils/test";
 import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Home Screen/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/tests/Header.test.jsx
@@ -0,0 +1,9 @@
+import {  render, screen } from "../utils/test";
+import Header from "../components/Header";
+import { BrowserRouter } from "react-router-dom";
+
+test("renders nav bar", () => {
+  render(<BrowserRouter><Header /></BrowserRouter>);
+  const linkElement = screen.getByAltText(/logo/i);
+  expect(linkElement).toBeInTheDocument();
+});
