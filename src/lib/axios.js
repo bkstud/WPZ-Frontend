20b7167fed92e3b00fe1d5e3b3c48be679da2f67
Bazyli Kot <ciobab97@gmail.com>
2022-05-15 17:27:25 +0200

    add URL variable for easy use in other modules

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const BACKEND_ADDRESS = process.env.REACT_APP_BACKEND_ADDRESS || "http://localhost"
 const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT || "3002"
+const URL = BACKEND_ADDRESS + ":" + BACKEND_PORT
 
 const http = axios.create({
-  baseURL: BACKEND_ADDRESS + ":" + BACKEND_PORT + "/api",
+  baseURL: URL + "/api",
   headers: {
     "Content-Type": "application/json",
   },
