2df7bb006040511766739c8495c39de3cb983766
bkstud <101209203+bkstud@users.noreply.github.com>
2022-05-16 00:21:35 +0200

    Env addresses ports (#14)
    * allow to change app port via env
    
    * add backend address:port env handle and documentation
    
    * add URL variable for easy use in other modules
    
    Co-authored-by: Bazyli Kot <ciobab97@gmail.com>

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const BACKEND_ADDRESS = process.env.REACT_APP_BACKEND_ADDRESS || "http://localhost"
+const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT || "3002"
+const URL = BACKEND_ADDRESS + ":" + BACKEND_PORT
+
 const http = axios.create({
-  baseURL: "http://localhost:3002/api", // Now it's local url
+  baseURL: URL + "/api",
   headers: {
     "Content-Type": "application/json",
   },
--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@ shopt -s extglob
 cp -r !(node_modules) /home/project
 cd /home/project
 npm install
+echo "REACT_APP_BACKEND_ADDRESS=${BACKEND_ADDRESS}" > .env
+echo "REACT_APP_BACKEND_PORT=${BACKEND_PORT}" >> .env
+export PORT="${PORT:-3000}"
 npm start
